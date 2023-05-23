d749f4da38b794f2d1ab7c3e6ddf2cb6564cd226
platformofai <platformofai@163.com>
2023-05-23 05:40:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import torch
 
 
 model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained(model_id)
 if torch.cuda.is_available():
     pipe = pipe.to("cuda")
 
